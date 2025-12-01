f5cad968490cd2e898e71da56f5a767c57f7f758
nguyenkhacbao206 <baonk.ptit@gmail.com>
2025-12-01 23:44:39 +0700

    fix bug avatar

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -61,7 +61,8 @@ public class SecurityConfig {
                     "/api/auth/**",
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
-                    "/swagger-ui.html"
+                    "/swagger-ui.html",
+                    "/avatars/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
