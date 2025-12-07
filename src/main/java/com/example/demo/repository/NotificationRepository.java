690c18bf27e971e549e0b39401d6720f06f29e15
nguyenkhacbao206 <baonk.ptit@gmail.com>
2025-12-07 22:31:07 +0700

    add feature notification

---
+++ src/main/java/com/example/demo/repository/NotificationRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+    List<Notification> findByReadFalseOrderByCreatedAtDesc();
+    List<Notification> findAllByOrderByCreatedAtDesc();
+}
